ced8930a16000aef1fa8372ba422f0a2a57cdb6d
rmakiyama <rmakiyama.io@gmail.com>
2019-08-26 17:38:43 +0900

    add watchable pattern

--- app/src/main/java/com/rmakiyama/edittextsample/MainActivity.kt
+++ app/src/main/java/com/rmakiyama/edittextsample/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.rmakiyama.edittextsample
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.appcompat.app.AppCompatActivity
+import com.rmakiyama.edittextsample.ui.SimpleTextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupWatchablePattern()
+    }
+
+    private fun setupWatchablePattern() {
+        input_watchable.addTextChangedListener(object : SimpleTextWatcher {
+            override fun afterTextChanged(editable: Editable) {
+                button_watchable.isEnabled =
+                    editable.length <= resources.getInteger(R.integer.limit_edit_text_max_length)
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/rmakiyama/edittextsample/ui/SimpleTextWatcher.kt
@@ -0,0 +1,10 @@
+package com.rmakiyama.edittextsample.ui
+
+import android.text.TextWatcher
+
+interface SimpleTextWatcher : TextWatcher {
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+}
